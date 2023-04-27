37a9b4c9b004f912198eee4df73b4e11985137d5
StefaniaPozzi <stefania.pozzi.it@gmail.com>
2023-04-27 15:50:39 +0200

    added events

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,32 +2,32 @@
 
 pragma solidity ^0.8.18;
 
-error Lottery_InsufficientTicketAmount(); //tracking state is cheaper with events than storing in a var
-
-contract Lottery{
-
+contract Lottery {
     uint256 private immutable i_ticketPrice;
-    address payable [] private s_players; //when one address wins, we have to pay him
+    address payable[] private s_players; //when one address wins, we have to pay him
+    error ErrorLottery__InsufficientTicketAmount();
 
-    constructor (uint256 ticketPrice){
-        i_ticketPrice = ticketPrice
+    event EventLottery__TicketBuyed(address); //tracking state is cheaper with events than storing in a var
+
+    constructor(uint256 ticketPrice) {
+        i_ticketPrice = ticketPrice;
     }
 
-    function buyTicket() public payable{
+    function buyTicket() public payable {
         if (msg.value < i_ticketPrice)
-            revert Lottery_InsufficientTicketAmount();
-        s_players.push((payable)msg.sender);
+            revert ErrorLottery__InsufficientTicketAmount();
+        s_players.push(payable(msg.sender));
+        emit EventLottery__TicketBuyed(msg.sender);
     }
 
-    function pickWinner(){}
+    function pickWinner() public {}
 
-//is not possible to automatise this? like lombok
-    function getTicketPrice() returns (uint256){
+    //is not possible to automatise this? like lombok
+    function getTicketPrice() public returns (uint256) {
         return i_ticketPrice;
     }
 
-    function getPlayers(uint256 index) return(address payable){
+    function getPlayers(uint256 index) public returns (address payable) {
         return s_players[index];
     }
-
 }
